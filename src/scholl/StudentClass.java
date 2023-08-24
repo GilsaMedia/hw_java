6a5a4cc6ec0d3072e452f210fa0c198b233cdc2b
gilsacoder <120129694+gilsacoder@users.noreply.github.com>
2023-08-24 19:11:27 +0300

    Student class

--- src/scholl/StudentClass.java
+++ src/scholl/StudentClass.java
@@ -1,6 +1,22 @@
-//public class StudentClass {
-//    char level;
-//    int number;
-//    int numOfStudents = 0;
-//    Student[] students = new Student[30];
-//}
+package scholl;
+
+public class StudentClass {
+    private char grade;
+    private int number;
+    private Teacher educator;
+    private int numOfStudent = 0;
+    private Student[] students = new Student[30];
+
+    public StudentClass(char grade, int number, Teacher educator, int numOfStudent, Student[] students) {
+        this.grade = grade;
+        this.number = number;
+        this.educator = educator;
+        this.numOfStudent = numOfStudent;
+        this.students = students;
+    }
+
+    public Student numInClass(Student[] students) {
+        Student student = students[number];
+        return student;
+    }
+}
--- src/scholl/main.java
+++ src/scholl/main.java
@@ -6,7 +6,12 @@ public class main {
         Person person2 = new Person();
         Student student1 = new Student("gil", "218478824", 85, 94, 87);
         Student student2 = new Student("zoe", "456734867", 100, 100, 100);
+        Student student3 = new Student("meow", "24143234", 32, 90, 100);
+        Student student4 = new Student("almog", "218131985",68 , 98, 55);
         Teacher teacher1 = new Teacher("neomi", "45366832", 20, true, "tanah, hebrew, tarbut");
+        Student[] students1 = {student1, student2, student3, student4};
+        StudentClass studentClass1 = new StudentClass('9', 7, teacher1, 4, students1);
+        studentClass1.numInClass(3);
 //        student1.setMath(100);
         System.out.println(person1.toString());
         System.out.println(person2.toString());
